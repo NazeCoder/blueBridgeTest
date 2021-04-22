1fe66c2b6c9feb4e35e599ccd5dc70803940bf39
NazeCoder <72634265+NazeCoder@users.noreply.github.com>
2021-04-22 23:10:21 +0800

    九数分三组更新完成
    全排列算法输出与迭代输出比较

--- _2015A03Demo.java
+++ _2015A03Demo.java
@@ -14,32 +14,43 @@ C = 3 * A
 public class _03DemoTest {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 1; j <= 9; j++) {
-				for (int k = 1; k <= 9; k++) {
-					if(i != j && i != k && j != k) {
-					list.add(i*100 + j*10 + k);	
-					}
-				}
-			}	
-		}
+		
+		ArraysList<Integer> list = list(9);
+		returnABC(list);
+		//A:192 219 273 327
+		
+		int[] temp = {1,2,3,4,5,6,7,8,9};
+		f(temp,0);
+
+	}
+	
+	static void returnABC(ArraysList<Integer> list){
+		
 		for (int a = 0; a < list.size(); a++) {
 			for (int b = 0; b < list.size(); b++) {
 				if(list.get(b) == 2 * list.get(a) && check(list.get(b), list.get(a))) {
 					for (int c = 0; c < list.size(); c++) {
 						if(list.get(c) == 3 * list.get(a) && check(list.get(b), list.get(c))&& check(list.get(c), list.get(a))) {
-							System.out.println("a: " + list.get(a) + " b: " + list.get(b) + " c: " + list.get(c)
-							);
+							System.out.println("a: " + list.get(a) + " b: " + list.get(b) + " c: " + list.get(c));
 						}
 					}
 				}
 			}
 		}
-		//192 219 273 327
-
 	}
-	
+	static ArrayList<Integer> list(int a){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+			for (int i = 1; i <= a; i++) {
+				for (int j = 1; j <= a; j++) {
+					for (int k = 1; k <= a; k++) {
+						if(i != j && i != k && j != k) {
+						list.add(i*100 + j*10 + k);	
+						}
+					}
+				}	
+			}
+		return list;
+	}
 	static boolean check(int a,int b) {
 		String stra = Integer.toString(a);
 		String strb = Integer.toString(b);
@@ -51,5 +62,28 @@ public class _03DemoTest {
 			}	
 		}
 		return true;
+	}
+	
+	static void f(int[] array,int k){
+		if( k == 9) {
+			int a = array[0] * 100 + array[1] * 10 + array[2];
+			int b = array[3] * 100 + array[4] * 10 + array[5];
+			int c = array[6] * 100 + array[7] * 10 + array[8];
+			if(b == 2 * a && c == 3 * a) {
+				System.out.println(a);
+			}
+		}
+		for (int i = k; i < array.length; i++) {
+			int t = array[i];
+			array[i] = array[k];
+			array[k] = t;
+			
+			f(array,k+1);
+			
+			t = array[i];
+			array[i] = array[k];
+			array[k] = t;
+			 
+		}
 	}
 }
